e80081446df81589027cd0d461f23e9b8250c6ad
xl000 <l_xb@foxmail.com>
2020-12-26 18:30:34 +0800

    io copy

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,5 @@
 use structopt::StructOpt;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io;
 use tokio::net::TcpListener;
 
 /// A basic example
@@ -19,25 +19,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let (mut socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
-            let mut buf = [0; 1024];
+            let (mut rd, mut wr) = socket.split();
 
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
+            if io::copy(&mut rd, &mut wr).await.is_err() {
+                eprintln!("failed to copy");
             }
         });
     }
